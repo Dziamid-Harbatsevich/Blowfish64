1c80ad48360b20c99e2bbfec665c0df5593dceaf
Dziamid Harbatsevich <newads777@gmail.com>
2023-09-25 16:01:44 +0300

    cleanup

--- Blowfish64Lib/Utils/Filesystem.cs
+++ Blowfish64Lib/Utils/Filesystem.cs
@@ -25,44 +25,6 @@ public static class Filesystem
 
     public static string? GetContentFromFile(string dir)
     {
-        //string initialDir = NewOrDefaultFolder(dir);
-
-        //// Configure open file dialog box
-        //var dialog = new Microsoft.Win32.OpenFileDialog();
-        //dialog.InitialDirectory = initialDir;
-        //dialog.FileName = "Document"; // Default file name
-        //dialog.DefaultExt = DEFAULT_FILE_EXTENSION; // Default file extension
-        //dialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
-
-        //// Show open file dialog box
-        //bool? result = dialog.ShowDialog();
-
-        //// Process open file dialog box results
-        //if (result == true)
-        //{
-        //    // Open document
-        //    string filename = dialog.FileName;
-        //    // Write the content to file
-        //    using (StreamReader inputFile = new StreamReader(filename))
-        //    {
-        //        var input = inputFile.ReadToEnd();
-        //        if (input.ToString().Length > 0)
-        //        {
-        //            return input.ToString();
-        //        }
-        //        else
-        //        {
-        //            MessageBox.Show("Содержимое отсутствует.",
-        //                "Error",
-        //                MessageBoxButton.OK,
-        //                MessageBoxImage.Error);
-        //        }
-        //    }
-        //}
-
-        //return null;
-
-
         string initialDir = NewOrDefaultFolder(dir);
 
         var dialog = new OpenFileDialog();
@@ -99,30 +61,6 @@ public static class Filesystem
 
     public static void PutContentToFile(string dir, string content, string fPrefix = "")
     {
-        //string initialDir = Filesystem.NewOrDefaultFolder(dir);
-
-        //// Configure save file dialog box
-        //var dialog = new Microsoft.Win32.SaveFileDialog();
-        //dialog.InitialDirectory = initialDir;
-        //dialog.FileName = fPrefix + DateTime.Now.ToFileTimeUtc();   // Default file name
-        //dialog.DefaultExt = DEFAULT_FILE_EXTENSION;     // Default file extension
-        //dialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
-
-        //// Show save file dialog box
-        //bool? result = dialog.ShowDialog();
-
-        //// Process save file dialog box results
-        //if (result == true)
-        //{
-        //    // Save document
-        //    string filename = dialog.FileName;
-        //    // Write the content to file
-        //    using (StreamWriter outputFile = new StreamWriter(filename))
-        //    {
-        //        outputFile.Write(content);
-        //    }
-        //}
-
         string initialDir = Filesystem.NewOrDefaultFolder(dir);
 
         SaveFileDialog dialog = new SaveFileDialog();
@@ -133,6 +71,5 @@ public static class Filesystem
 
         if (dialog.ShowDialog() == true)
             File.WriteAllText(dialog.FileName, content, Encoding.Default);
-
     }
 }
